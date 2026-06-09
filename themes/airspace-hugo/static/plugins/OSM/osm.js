eb39cf95d81dd84faa657f86a965ac351ad02426
Fraleroy <63159187+Fraleroy@users.noreply.github.com>
2026-06-09 19:01:51 +0200

    Modif code OSM

--- themes/airspace-hugo/static/plugins/OSM/osm.js
+++ themes/airspace-hugo/static/plugins/OSM/osm.js
@@ -1,48 +1,47 @@
 window.marker = null;
 
-function initialize() {
+function initializeOSMMap() {
   var $mapCanvas = $('#map_canvas');
   var latitude = parseFloat($mapCanvas.attr('data-latitude'));
   var longitude = parseFloat($mapCanvas.attr('data-longitude'));
   var mapMarker = $mapCanvas.attr('data-marker');
   var mapMarkerName = $mapCanvas.attr('data-marker-name');
 
-  // Initialiser la carte avec un style grayscale (tuiles OSM noir et blanc)
+  // Initialiser la carte avec style grayscale
   var map = L.map('map_canvas', {
     zoomControl: true,
-    attributionControl: false, // Désactivé pour coller au style original
-    scrollWheelZoom: false      // Désactive le zoom par molette (comme Google Maps par défaut)
+    scrollWheelZoom: false,
+    attributionControl: false
   }).setView([latitude, longitude], 15);
 
-  // Ajouter les tuiles OSM en grayscale
+  // Tuiles OSM en noir et blanc (style grayscale)
   L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
   }).addTo(map);
 
-  // Désactiver les contrôles inutiles (comme dans ton code Google Maps)
-  map.removeControl(map.zoomControl); // On réactive zoomControl plus bas
-  map.zoomControl = L.control.zoom({
-    position: 'topright' // Correspond à ZoomControlStyle.LARGE
+  // Réactiver zoomControl (comme dans ton code original)
+  L.control.zoom({
+    position: 'topright'
   }).addTo(map);
 
-  // Créer l'icône personnalisée (30x50px comme dans ton code)
-  var pinIcon = L.icon({
-    iconUrl: mapMarker,
-    iconSize: [30, 50],
-    iconAnchor: [15, 50], // Point d'ancrage au centre-bas de l'icône
-    popupAnchor: [0, -50] // Position de la popup par rapport à l'icône
-  });
-
-  // Ajouter le marqueur
-  marker = L.marker([latitude, longitude], {
-    icon: pinIcon,
-    title: mapMarkerName
-  }).addTo(map);
+  // Marqueur personnalisé (30x50px comme dans gmap.js)
+  if (mapMarker) {
+    var pinIcon = L.icon({
+      iconUrl: mapMarker,
+      iconSize: [30, 50],
+      iconAnchor: [15, 50],
+      popupAnchor: [0, -50]
+    });
+    marker = L.marker([latitude, longitude], {
+      icon: pinIcon,
+      title: mapMarkerName
+    }).addTo(map);
+  }
 }
 
 // Initialisation
 if (document.getElementById('map_canvas') !== null) {
   $(document).ready(function() {
-    initialize();
+    initializeOSMMap();
   });
 }
